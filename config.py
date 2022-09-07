0d9607b34d83da65fbdc8e7c267f903147407737
keyun <93893819+Keyunfung@users.noreply.github.com>
2022-09-07 16:54:50 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cpsb9o3bch7v.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "fungkeyun-employee"
+customregion = "us-east-1"
